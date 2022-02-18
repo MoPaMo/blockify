4e25b9249c03a78395dc98c2ca543c047fd87dd5
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2022-02-18 19:04:11 +0100

    add sqlite3

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT != undefined ? process.env.PORT : 8080;
+const sqlite3 = require("sqlite3").verbose();
+
+var dberror = false;
+let db = new sqlite3.Database("./db.db", sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.error(err.message);
+    dberror = err.message;
+  }
+  console.log("Connected to the database.");
+});
 
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);

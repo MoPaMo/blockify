a2aa1ccee81ad0488d161be8ebcb358d1ccef98d
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2022-02-18 18:15:41 +0100

    send index file

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT != undefined ? process.env.PORT : 8080;
 
-
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/views/index.html`);
+});
 app.listen(port, function(err) {
   if (err) console.log(err);
   console.log("Server listening on PORT", port);
